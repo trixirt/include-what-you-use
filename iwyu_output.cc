f976e44b87ae42c8aae862c99a80e77192c31aa6
Tom Rix <tom.rix@sony.com>
2019-01-15 13:49:47 -0800

    Fiddle with output string

--- iwyu_output.cc
+++ iwyu_output.cc
@@ -1838,8 +1838,12 @@ OutputLine PrintableIncludeOrForwardDeclareLine(
     return OutputLine(line.line());
   }
 
-  return OutputLine(line.line(),
-    GetSymbolsSortedByFrequency(line.symbol_counts()));
+  if (GlobalFlags().no_reorder) {
+    return OutputLine(line.line() + "  // lines " + line.LineNumberString());
+  } else {
+    return OutputLine(line.line(),
+		      GetSymbolsSortedByFrequency(line.symbol_counts()));
+  }
 }
 
 enum class LineSortOrdinal {
@@ -1955,6 +1959,10 @@ size_t PrintableDiffs(const string& filename,
       if (line->is_desired() && !line->is_present()) {
         output_lines.push_back(
           PrintableIncludeOrForwardDeclareLine(*line, aqi));
+
+	if (GlobalFlags().no_reorder)
+	  output_lines.back().add_prefix("+ ");
+
         ++num_edits;
       }
     }
