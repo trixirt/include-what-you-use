be93b4284f5f64e60e5b389f16764c749a0db46a
Tom Rix <tom.rix@sony.com>
2019-01-19 07:17:47 -0800

    Fix replacements tag

--- iwyu_output.cc
+++ iwyu_output.cc
@@ -2113,10 +2113,9 @@ size_t PrintableDiffs(const string& filename,
 	  }
 	  fprintf(f, "    </lines>\n");
 	  fprintf(f, "  </replacement>\n");
-	  fprintf(f, "</replacements>\n");
 	}
       }
-      
+      fprintf(f, "</replacements>\n");
       fprintf(f, "</file>\n");
       fprintf(f, "</files>\n");
       fprintf(f, "</iwyu>\n");
